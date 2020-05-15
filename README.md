5ef64f43c1453b66a0909dbeb626434bc27c54f9
gondone666 <64283101+gondone666@users.noreply.github.com>
2020-05-15 15:16:57 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # h2-wrapper
 An HTTP2/1.1 client with SOKCS5 support
 ###### VERSION
+**0.0.4** - Normalize request arguments (url, options) or (url) or (options)
+**0.0.3** - Tough Cookie support
+**0.0.2** - HTTP1.1 support
 **0.0.1** - HTTP2/SOCKS5 Support
+
 ###### TODO
 - [x] HTTP2/SOCKS5 Support
-- [ ] HTTPS1/1.1 Support
+- [x] HTTPS/1.1 Support
 - [ ] Plain HTTP Support
 - [ ] .....................
 ###### Example
 ```javascript
 const h2 = require('h2-wrapper');
+const tough = require("tough-cookie");
 
 (async () => {
-	const nike = await h2.extend({
+	const jar = new tough.CookieJar();
+	const nike = new h2({
 		headers: {
 			"user-agent": "Mozilla/5.0 (Linux; Android 9; ZTE Blade A5 2019RU) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.117 Mobile Safari/537.36",
 			"accept-encoding": "gzip, deflate, br",
@@ -21,11 +27,16 @@ const h2 = require('h2-wrapper');
 			"sec-fetch-mode": "cors",
 			"sec-fetch-dest": "empty"
 		},
-		proxy: { host: '127.0.0.1', port: 8888, type: 5 },
+		jar: jar,
+		proxy: { host: '127.0.0.1', port: 54989, type: 5 },
 		ciphers: "ECDHE-ECDSA-AES128-GCM-SHA256 ECDHE-RSA-AES128-GCM-SHA256 ECDHE-ECDSA-AES256-GCM-SHA384 ECDHE-RSA-AES256-GCM-SHA384 ECDHE-ECDSA-CHACHA20-POLY1305-SHA256 ECDHE-RSA-CHACHA20-POLY1305-SHA256 ECDHE-RSA-AES128-SHA ECDHE-RSA-AES256-SHA RSA-AES128-GCM-SHA256 RSA-AES256-GCM-SHA384 RSA-AES128-SHA RSA-AES256-SHA RSA-3DES-EDE-SHA",
 	})
-	var res = await nike('https://www.nike.com/');
-	console.log(res.body);
+	var res1 = await nike.request('https://www.nike.com/ru/');
+	console.log(res1.httpVersion);
+	console.log(res1.jar);
+	var res2 = await nike.request('https://secure-global.nike.com/');
+	console.log(res2.httpVersion);
+	console.log(res2.jar);
 	nike.close();
 })();
 ```
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http2 = require("http2")
+const https = require("https")
 const tough =  require("tough-cookie");
 const uri = require("url");
 const querystring = require("querystring");
@@ -6,15 +7,83 @@ const zlib = require("zlib");
 const merge = require("deepmerge")
 const SocksClient = require("socks").SocksClient;
 const tls = require("tls");
+const clone = require("clone")
 
-class H2Agent {
+class Session {
+	constructor(version) {
+		this.socket = false;
+		this.version = version;
+	}
+	close() {
+		this.socket.destroy();
+	}
+}
+
+class H1Session extends Session {
+	constructor() {
+		super('http1.1');
+	}
+	request(options) {
+		let opts = { ...options }
+		opts.createConnection = () => this.socket;
+		return https.request(opts);
+	}
+	connect(options, socket)  {
+		return new Promise((resolve, reject) => {
+			this.socket = socket;
+			this.options = options
+			resolve();
+		})
+	}
+	reorderHeaders(options) {
+		let reorder = {
+			"host": options.headers["host"] ? options.headers["host"] : options.host,
+			"connection": "keep-alive"
+		}
+		for (var key in options.headers) {
+			if (!reorder[key]) reorder[key] = options.headers[key];
+		}
+		return reorder;
+	}
+}
+
+class H2Session extends Session {
+	constructor() {
+		super('h2');
+	}
+	request(options) {
+		return this.socket.request(options.headers);
+	}
+	connect(options, socket) {
+		return new Promise((resolve, reject) => {
+			this.socket = http2.connect(options, {
+				createConnection: () => socket
+			}, () => resolve())
+		})
+	}
+	reorderHeaders(options) {
+		let reorder = {
+			":method": options.headers[":method"] ? options.headers[":method"] : options.method,
+			":authority": options.headers[":authority"] ? options.headers[":authority"] : options.host,
+			":scheme": options.headers[":scheme"] ? options.headers[":scheme"] : options.protocol.slice(0, -1),
+			":path": options.headers[":path"] ? options.headers[":path"] : options.path,
+			"origin": options.headers["origin"] ? options.headers["origin"] : options.origin
+		}
+		for (var key in options.headers) {
+			if (!reorder[key]) reorder[key] = options.headers[key];
+		}
+		return reorder;
+	}
+}
+
+class HWrapper {
 	constructor(options){
 		this.sessions = [];
 		this.options = options || {};
 		if (!this.options.jar) {
 			this.options.jar = new tough.CookieJar();
 		}
-		this.options.ALPNProtocols = ['h2'];
+		this.options.ALPNProtocols = ['h2', 'http/1.1', 'http/1.0'];
 		this.res = false;
     }	
 	tlsSession(options) {
@@ -32,20 +101,19 @@ class H2Agent {
 						options.socket = info.socket;
 						options.settings = {};
 						const socket = tls.connect(options, () => {
-							self.sessions[options.origin] = http2.connect(options, {
-								createConnection: () => socket
-							}, () => resolve(self.sessions[options.origin]))
+							if (socket.alpnProtocol == 'h2') {
+								self.sessions[options.origin] = new H2Session();
+							} else {
+								self.sessions[options.origin] = new H1Session();
+							}
+							self.sessions[options.origin].connect(options, socket).then(()=>resolve());
 						});
 					});
 				} catch (err) {
 					reject(err)
 				}
 			} else {
-				const socket = tls.connect(options, () => {
-					self.sessions[options.origin] = http2.connect(options, {
-						createConnection: () => socket
-					}, () => resolve(self.sessions[options.origin]))
-				});
+				// todo non proxied requests
 			}
 		});
 	}
@@ -91,7 +159,7 @@ class H2Agent {
 	setCookies() {
 		const self = this;
 		return new Promise((resolve, reject) => {
-			let headers = self.res.headers ? self.res.headers : {};
+			let headers = self.res.headers ? self.res.headers : {}, cookies=[];
 			if (!headers['set-cookie']) {
 				resolve();
 			}
@@ -101,7 +169,7 @@ class H2Agent {
 				cookies = [tough.Cookie.parse(headers['set-cookie'])];
 			}
 			cookies.forEach((c) => {
-				self.options.jar.setCookieSync(c, self.options.url, (error) => {
+				self.options.jar.setCookieSync(c, self.res.url, (error) => {
 					if (error) throw new Error(error)
 				})
 			})
@@ -109,61 +177,69 @@ class H2Agent {
 		});
 	}
 
-
-	request(options) {
+	request(url, opts = {}) {
 		const self = this;
+		let options = {};
+		if (typeof url === "string" && typeof opts === "object") {
+			options = { ...opts, ...uri.parse(url), url };
+
+		} else if (typeof url === "object") {
+			options = { ...opts,...url, url: url.toString() };
+		}
+		if (!options.method) {
+			options.method = 'GET';
+		}
+		if (!options.url) {
+			throw new Error("Missing URL param");
+		}
+		if (!options.port) options.port = options.protocol.slice(0, -1) === 'https' ? 443 : 80;
+		options.servername = options.host;
+		options.origin = options.protocol + '//' + options.host;
+		options = merge(options, self.options, {
+			clone: false
+		})
 		this.res = new Promise((resolve, reject) => {
 			function finish() {
 				Promise.all([self.decodeReseponse(), self.setCookies()]).then(() => resolve(self.res));
 			}
-			options = merge(options, self.options, {
-				clone: false
-			})
-			options = { ...uri.parse(options.url), ...options };
-			if (!options.port) options.port = options.protocol.slice(0, -1) === 'https' ? 443 : 80;
-			options.servername = options.host;
-			options.origin = options.protocol + '//' + options.host;
-			var headers = {
-				":method": options.headers[":method"] ? options.headers[":method"] : options.method,
-				":authority": options.headers[":authority"] ? options.headers[":authority"] : options.host,
-				":scheme": options.headers[":scheme"] ? options.headers[":scheme"] : options.protocol.slice(0, -1),
-				":path": options.headers[":path"] ? options.headers[":path"] : options.path,
-				"origin": options.headers["origin"] ? options.headers["origin"] : options.origin
-			}
-			for (var key in options.headers) {
-				if (!headers[key]) headers[key] = options.headers[key];
-			}
 			options.jar.getCookieString(options.url, (error, cookie) => {
 				if (error) throw new Error(error);
-				if (cookie.length > 0) headers["cookie"] = cookie;
+				if (cookie.length > 0) options.headers["cookie"] = cookie;
 			})
-			self.tlsSession(options).then((session) => {
+			self.tlsSession(options).then(() => {
 				if (!options.body) options.body = "";
 				if (["POST", "PATCH"].includes(options.method)) {
 					if (options.form) {
 						options.body = querystring.stringify(options.form);
-						headers["content-type"] = headers["content-type"] ? headers["content-type"] : "application/x-www-form-urlencoded";
+						options.headers["content-type"] = options.headers["content-type"] ? options.headers["content-type"] : "application/x-www-form-urlencoded";
 
 					} else if (options.json) {
 						options.body = JSON.stringify(options.json);
-						headers["content-type"] = headers["content-type"] ? headers["content-type"] : "application/json";
+						options.headers["content-type"] = options.headers["content-type"] ? options.headers["content-type"] : "application/json";
 					}
-					headers["content-length"] = options.body.length;
+					options.headers["content-length"] = options.body.length;
 				}
-				options.headers = headers;
-				console.log(headers, options.body)
-				var req = session.request(headers);
+				options.headers = self.sessions[options.origin].reorderHeaders(options);
+				var req = self.sessions[options.origin].request(options);
 				if (["POST", "PATCH"].includes(options.method)) {
 					req.end(options.body);
 				} else {
 					req.end();
 				}
-				req.on("response", (headers) => {
-					self.res = { headers, req, data:[]};
-					req.on('data', function (chunk) {
-						self.res.data.push(chunk);
-					});
-					req.on('end', finish);
+				req.on("response", (res) => {
+					if (self.sessions[options.origin].version == 'h2') {
+						self.res = { headers: res, data: [], url: options.url, httpVersion: self.sessions[options.origin].version, jar:options.jar};
+						req.on('data', function (chunk) {
+							self.res.data.push(chunk);
+						});
+						req.on('end', finish);
+					} else {
+						self.res = { headers: res.headers, data: [], url: options.url, httpVersion: self.sessions[options.origin].version, jar: options.jar };
+						res.on('data', function (chunk) {
+							self.res.data.push(chunk);
+						});
+						res.on('end', finish);
+                    }
 				});
 				req.on("error", function (err) {
 					throw new Error(err)
@@ -180,30 +256,4 @@ class H2Agent {
     }
 }
 
-const request = async function (url, opts = {}) {
-	let options = {};
-	if (typeof url === "string" && typeof opts === "object") {
-		options = { ...opts, url };
-		
-	} else if (typeof url === "object") {
-		options = { ...url };
-	}
-	if (!options.method) {
-		options.method = 'GET';
-    }
-	if (!options.url) {
-		throw new Error("Missing URL param");
-	}
-	return request.agent.request(options);
-}
-
-request.extend = (defaults = {}) => {
-	let req = request;
-	req.agent = new H2Agent(defaults);
-	return req;
-}
-request.close=() => {
-	request.agent.close();
-}
-
-module.exports = request
+module.exports = HWrapper
